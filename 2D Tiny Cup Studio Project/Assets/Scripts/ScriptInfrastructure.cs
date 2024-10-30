c360b1d94186a25f364e8409990c90bd4e201534
Holeera <lazarvaragictinycupstudios@gmail.com>
2024-10-30 18:54:05 +0100

    Update 0.5
    - Streets 1 & 2

--- 2D Tiny Cup Studio Project/Assets/Scripts/ScriptInfrastructure.cs
+++ 2D Tiny Cup Studio Project/Assets/Scripts/ScriptInfrastructure.cs
@@ -2,11 +2,15 @@ using AC;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
 
 public class ScriptInfrastructure : MonoBehaviour
 {
+    
     public static ScriptInfrastructure instance;
-
+    
     private void Awake()
     {
         instance = this;
@@ -14,6 +18,13 @@ public class ScriptInfrastructure : MonoBehaviour
 
     private void Start()
     {
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Streets2");
+        }
     }
 }
